d9ac996227f2254217f4c1a8d6f8f50d3c0f0eae
Jake Levi <jake.levi@hotmail.com>
2024-02-10 17:53:08 -0500

    Added Bee's stamina bar

---
+++ Scripts/Stamina.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+@onready var _animated_sprite = $AnimatedSprite2D
+
+var tot_frames = 13
+
+func _ready():
+	_animated_sprite.pause()
+	
+func _on_shrimpball_stamina(val):
+	if val > 0:
+		_animated_sprite.set_frame(floor(val / 7))
+	elif val >= 91:
+		_animated_sprite.set_frame( 13 )
+	else:
+		_animated_sprite.set_frame( 0 )
