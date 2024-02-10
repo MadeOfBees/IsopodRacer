142818a517d6082f35e4a2893797800c360a69cf
Jake Levi <jake.levi@hotmail.com>
2024-02-10 13:38:09 -0500

    Polished shrimpball character

--- Shrimpball.gd
+++ Shrimpball.gd
@@ -1,11 +1,14 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Make sure the physics process is enabled to check for input in physics frames
+	set_physics_process(true)
 
+func _physics_process(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		teleport_to_mouse_cursor()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func teleport_to_mouse_cursor():
+	var mouse_position = get_global_mouse_position()
+	position = mouse_position
+	linear_velocity = Vector2.ZERO  # Optional: Reset velocity to avoid unintended movement after teleporting
