990e3da7fca15374077bcbd184dad99fb3bcc010
Jake Levi <jake.levi@hotmail.com>
2024-02-10 21:05:38 -0500

    Added custom cursors and stopped player from holding down rmb for infinite magnet

--- Scripts/Shrimpball.gd
+++ Scripts/Shrimpball.gd
@@ -6,6 +6,11 @@ var stamina_val = 80
 var stamina_inc_step = 15
 var stamina_dec_step = 1
 
+
+# Load the custom images for the mouse cursor.
+var mouse = load("res://Assets/mouse_sm.png")
+var magnet = load("res://Assets/mouse_magnet_sm.png")
+
 signal stamina(val)
 
 func _process(delta):
@@ -14,6 +19,7 @@ func _process(delta):
 	stamina.emit(stamina_val)
 
 func _ready():
+	Input.set_custom_mouse_cursor(mouse)
 	set_physics_process(true)
 
 func _integrate_forces(state: PhysicsDirectBodyState2D):
@@ -21,13 +27,11 @@ func _integrate_forces(state: PhysicsDirectBodyState2D):
 	var direction_to_mouse = (mouse_position - state.transform.origin).normalized()
 	var force = direction_to_mouse * attraction_strength
 	if Input.is_action_pressed("game_magnet") and stamina_val > 0:
-		state.apply_central_impulse(force)
 		stamina_val -= stamina_dec_step
+		if stamina_val > 1: 
+			state.apply_central_impulse(force)
+			Input.set_custom_mouse_cursor(magnet)
 	else:
 		var zero_force = Vector2(0,0)
 		state.set_constant_force(zero_force)
-
-
-
-func _input(event):
-	print(event.as_text())
+		Input.set_custom_mouse_cursor(mouse)
--- Scripts/Stamina.gd
+++ Scripts/Stamina.gd
@@ -7,6 +7,9 @@ var tot_frames = 13
 func _ready():
 	_animated_sprite.pause()
 	
+func handlestamina(stamina):
+	print("Stamina "+ stamina)
+	
 func _on_shrimpball_stamina(val):
 	if val > 0:
 		_animated_sprite.set_frame(floor(val / 7))
